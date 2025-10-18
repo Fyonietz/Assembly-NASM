58b6065fd070bcfc972d04a82cb0dcc811efcc18
Habib Herdiansyah <expduta@gmail.com>
2025-10-18 11:22:53 +0700

    Belajar Kali

--- Variable/times.asm
+++ Variable/times.asm
@@ -1,22 +1,22 @@
 section .data
-  number1 dq 20
-  number2 dq 14 
+  number1 dq 2032131
+  number2 dq 1241231
   result dq 0
-
-  outbuf db '000000000',0xA
-  outbuflen equ 11
-
+  
+  buffer db '000000000',0xA
 
 section .text
   global _start
 
+
 _start:
   mov rax,[number1]
-  sub rax,[number2]
+  mov rbx,[number2]
+  mul rbx
   mov [result],rax
 
   mov rcx,10
-  mov rbx,outbuf+9
+  mov rbx,buffer+9
   mov rdx,0
 
 convert_loop:
@@ -28,15 +28,13 @@ convert_loop:
   test rax,rax
   jnz convert_loop
 
-  mov rax,1
-  mov rdi,1
+  mov rax,1 
+  mov rdi,1 
   mov rsi,rbx
-  lea rdx,[outbuf+9]
+  lea rdx,[buffer+9]
   sub rdx,rbx
   syscall
 
   mov rax,60
   xor rdi,rdi
   syscall
-
-
